06b555a2dfd560f76390c3e8b45fc75e7705235a
cCarlos <ciriastorres@gmail.com>
2024-01-31 19:39:52 -0600

    Modifies the code tho show the empty window

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 import pygame
 
-pygame.init()
+class PyPongGame:
+    def __init__(self):
+        pygame.init()
+        self.size = (800, 600)
+        self.screen = pygame.display.set_mode(self.size)
+        pygame.display.set_caption("My Pypong Game")
+        self.running = True
 
-size = (800, 600)
-screen = pygame.display.set_mode(size)
+    def run_game(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            self.update_screen()
 
-pygame.display.set_caption("My Pypong game")
+    def update_screen(self):
+        pygame.display.flip()
 
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+game = PyPongGame()
+game.run_game()
