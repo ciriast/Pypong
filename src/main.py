66913b5579dec30da244eabe510f3719dcc69b72
cCarlos <ciriastorres@gmail.com>
2024-02-02 20:32:50 -0600

    Only prints one pixel in the screen,

--- src/main.py
+++ src/main.py
@@ -9,15 +9,16 @@ class PyPongGame:
         pygame.display.set_caption("My Pypong Game")
         self.running = True
 
+        my_x = random.randint(0, 800-1)
+        my_y = random.randint(0, 600-1)
+
+        pixel = random.randint(0, 255)
+
+        self.screen.set_at((my_x, my_y), (pixel))
+        
     def run_game(self):
         while self.running:
-            my_x = random.randint(0, 800-1)
-            my_y = random.randint(0, 600-1)
-
-            pixel = random.randint(0, 255)
-
-            self.screen.set_at((my_x, my_y), (pixel))
-
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
