b9fd1e2ab31876493f9f5440cc7d5de2360b657b
cCarlos <ciriastorres@gmail.com>
2024-02-03 12:08:53 -0600

    Improve code readability

--- src/main.py
+++ src/main.py
@@ -1,24 +1,25 @@
 import pygame
 import random
 
+WIDTH = 800
+HEIGHT = 600
+
 class PyPongGame:
     def __init__(self):
         pygame.init()
-        self.size = (800, 600)
+        self.size = (WIDTH, HEIGHT)
         self.screen = pygame.display.set_mode(self.size)
         pygame.display.set_caption("My Pypong Game")
         self.running = True
 
-        my_x = random.randint(0, 800-1)
-        my_y = random.randint(0, 600-1)
+        random_x = random.randint(0, WIDTH)
+        random_y = random.randint(0, HEIGHT)
+        random_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-        pixel = random.randint(0, 255)
-
-        self.screen.set_at((my_x, my_y), (pixel))
+        self.screen.set_at((random_x, random_y), (random_color))
         
     def run_game(self):
         while self.running:
-            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
